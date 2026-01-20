d480295b4e9b391dba3d3c33656828b1db352640
D7OM <apyt5257@gmail.com>
2026-01-20 15:01:22 +0300

    Fix formatting and add comment in config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  roleId: "1463099095223898217",
+  roleId: "1463099095223898217", // ID الرتبة
   gif: "https://cdn.discordapp.com/attachments/1454655783005454379/1463101026994491453/bc41db78d6a02d2f3620f9219712321d.gif",
-
   happyEmojis: [
     "<a:incwannasaysmh:1454703728518103093>",
     "<a:cathearts:1463096379856654483>",
@@ -9,7 +8,6 @@ module.exports = {
     "<a:TH_Smile:1463095270635212830>",
     "<a:6nervous:1463096587063525468>"
   ],
-
   sadEmojis: [
     "<a:bocchitherock:1458615786825318667>",
     "<a:Fuckyou:1457762508755697776>",
