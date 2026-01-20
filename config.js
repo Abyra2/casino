482a72186de40348ce9e6879347ba73449db1e94
D7OM <apyt5257@gmail.com>
2026-01-20 13:58:19 +0300

    Add configuration for emojis and role ID

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  roleId: "1463099095223898217",
+  gif: "https://cdn.discordapp.com/attachments/1454655783005454379/1463101026994491453/bc41db78d6a02d2f3620f9219712321d.gif",
+
+  happyEmojis: [
+    "<a:incwannasaysmh:1454703728518103093>",
+    "<a:cathearts:1463096379856654483>",
+    "<a:Yakitori_emoji28:1463096294980452519>",
+    "<a:TH_Smile:1463095270635212830>",
+    "<a:6nervous:1463096587063525468>"
+  ],
+
+  sadEmojis: [
+    "<a:bocchitherock:1458615786825318667>",
+    "<a:Fuckyou:1457762508755697776>",
+    "<:tomato_ohno:1457346341490524354>",
+    "<a:Angerrr:1463096649726296241>"
+  ]
+};
