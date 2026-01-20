898e91d0eb4786d5ae7dea5559671e48655eb7c5
D7OM <apyt5257@gmail.com>
2026-01-20 15:18:20 +0300

    Change database file path from roles.db to data.db

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
-const Database = require("better-sqlite3");
-const db = new Database("roles.db");
+const Database = require('better-sqlite3');
+const db = new Database('./data.db');
 
 db.prepare(`
-CREATE TABLE IF NOT EXISTS sinless (
-  user_id TEXT,
-  guild_id TEXT,
-  status TEXT
-)
+  CREATE TABLE IF NOT EXISTS sinless (
+    user_id TEXT,
+    guild_id TEXT,
+    status TEXT
+  )
 `).run();
 
 module.exports = db;
